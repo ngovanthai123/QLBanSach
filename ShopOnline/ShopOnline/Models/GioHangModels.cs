3cdb575e197049d0b1df5b5d1af7824e0fde75a3
ngovanthai123 <ngothai781@gmail.com>
2021-10-24 15:35:27 +0700

    Create Giỏ Hàng

---
+++ ShopOnline/ShopOnline/Models/GioHangModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models
+{
+    public class SanPhamTrongGio
+    {
+        public Sach sAch { get; set; }
+        public int soLuongTrongGio { get; set; }
+    }
+}
